e152c8f9f20ece09dc465765a1ad37342e968720
mgoubran <magedgoubran@gmail.com>
2017-08-12 22:21:28 -0700

    add all models opt & one type of model

--- quickpaint.py
+++ quickpaint.py
@@ -9,9 +9,11 @@ from collections import defaultdict
 from scipy.misc import imread, imsave
 import time
 import os
+import glob
 import tensorflow as tf
 
-os.environ['TF_CPP_MIN_LOG_LEVEL']="3"  # filter out info & warning logs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"  # filter out info & warning logs
+
 
 # read input arguments
 def get_opts():
@@ -35,16 +37,14 @@ def get_opts():
                         metavar='', default='/gpu:0')
     parser.add_argument('-b', '--batch-size', type=int,
                         dest='batch_size', help='batch size for feed-forwarding (default: %(default)s)',
-                        metavar='', default=4)
-    parser.add_argument('-a', '--model-arch', type=str,
-                        dest='model_arch',
-                        help='model architecture if models in form (.model) are used, (default: %(default)s)',
-                        metavar='', default='pre-trained_models/model.meta')
+                        metavar='', default=2)
     parser.add_argument('-ma', '--mask', type=int,
-                        dest='mask', help='create binary mask from input (@ 1 percent of max) and mask output, (default: %(default)s)',
+                        dest='mask',
+                        help='create binary mask from input (@ 1 percent of max) and mask output, (default: %(default)s)',
                         metavar='', default=0)
     parser.add_argument('-bl', '--blend', type=float,
-                        dest='blend', help='multiply the original image with the output using a weighting (factor), (default: %(default)s)',
+                        dest='blend',
+                        help='multiply the original image with the output using a weighting (factor), (default: %(default)s)',
                         metavar='', default=0)
 
     opts = parser.parse_args()
@@ -52,17 +52,18 @@ def get_opts():
     # check inputs
     assert os.path.exists(opts.in_path), 'Input dir: %s does not exist!' % opts.in_path
 
-    if os.path.isdir(opts.out_path):
+    if "." not in opts.out_path:
         if not os.path.exists(opts.out_path):
             print('creating output dir')
             os.makedirs(opts.out_path)
 
-    assert os.path.exists(opts.model_path), 'Model not found.. %s does not exist!' % opts.model_path
+    # if opts.model_path!="all":
+    # 	assert os.path.exists(opts.model_path), 'Model not found.. %s does not exist!' % opts.model_path
 
     assert isinstance(opts.batch_size, int), '-b, --batch_size needs to be a positive integer'
     assert opts.batch_size > 0, '-b, --batch_size needs to be a positive integer'
     assert isinstance(opts.device, str)
-    assert (opts.mask==1 or opts.mask==0), '-ma, --mask needs to be binary'
+    assert (opts.mask == 1 or opts.mask == 0), '-ma, --mask needs to be binary'
     assert opts.blend <= 1, '-bl, --blend needs to be a float equal or less to 1'
 
     return opts
@@ -77,15 +78,15 @@ def read_img(src):
     return img
 
 
-def eval(data_in, paths_out, model_path, device='/gpu:0', batch_size=4,
-         model_arch='pre-trained_models/model.meta', mask=0, blend=0):
+def eval(data_in, paths_out, model_path, device, batch_size,
+         mask, blend):
     '''
     Transfers image style to another image using feed-forwarding and a pre-trained model
 
     :param data_in: List of input content images (having same shape)
     :param paths_out: List of output paths
     :param model_path: Path for input pre-trained model (either .ckpt or .model)
-        for .model models will read model meta graph from pre-trained_modes/model.meta
+        for .model models will read model meta graph from pre-trained_models/model.meta
     :param device: GPU to use for computation
     :param batch_size: Number of images batched (def: 4) or # of images if smaller
     
@@ -112,29 +113,14 @@ def eval(data_in, paths_out, model_path, device='/gpu:0', batch_size=4,
 
         batch_shape = (batch_size,) + img_shape
 
-        model_ext = os.path.splitext(model_path)[1]
+        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape, name='img_placeholder')
 
-        if model_ext == ".ckpt":
+        # get predictions from model
+        preds = transform.net(img_placeholder)
+        saver = tf.train.Saver()
 
-            img_placeholder = tf.placeholder(tf.float32, shape=batch_shape, name='img_placeholder')
-
-            # get predictions from model
-            preds = transform.net(img_placeholder)
-            saver = tf.train.Saver()
-            # restore model
-            saver.restore(sess, model_path)
-
-        else:
-
-            # print("\n Reading model architecture from %s" % opts.model_arch)
-
-            # load model meta graph
-            saver = tf.train.import_meta_graph(model_arch, clear_devices=True)
-            # restore model
-            saver.restore(sess, model_path)
-
-            img_placeholder = tf.get_collection("inputs")[0]
-            preds = tf.get_collection("output")[0]
+        # restore model
+        saver.restore(sess, model_path)
 
         num_iters = int(len(paths_out) / batch_size)
 
@@ -153,20 +139,20 @@ def eval(data_in, paths_out, model_path, device='/gpu:0', batch_size=4,
 
             # save output images
             for j, path_out in enumerate(curr_batch_out):
-                img = np.clip(_preds[j], 0, 255).astype(np.uint8) # after clipping to 255
+                img = np.clip(_preds[j], 0, 255).astype(np.uint8)  # after clipping to 255
 
                 if mask == 1:
                     thr = X[i].max() * 0.01
-                    inmask = np.where(X[j]>thr, 1, 0)
+                    inmask = np.where(X[j] > thr, 1, 0)
                     if inmask.shape != img.shape:
-                        img = img[0:inmask.shape[0],0:inmask.shape[1],:]
-                    img = np.multiply(inmask,img)                        
-                
+                        img = img[0:inmask.shape[0], 0:inmask.shape[1], :]
+                    img = np.multiply(inmask, img)
+
                 if blend > 0:
                     inimg = X[i] * blend
                     if inimg.shape != img.shape:
-                        img = img[0:inimg.shape[0],0:inimg.shape[1],:]
-                    img = np.multiply(inimg,img)
+                        img = img[0:inimg.shape[0], 0:inimg.shape[1], :]
+                    img = np.multiply(inimg, img)
 
                 imsave(path_out, img)
 
@@ -179,7 +165,7 @@ def eval(data_in, paths_out, model_path, device='/gpu:0', batch_size=4,
              device=device, batch_size=1)
 
 
-def eval_mul_dims(in_path, out_path, model_path, device, batch_size, model_arch, mask, blend):
+def eval_mul_dims(in_path, out_path, model_path, device, batch_size, mask, blend):
     '''
     Runs "eval" on diff image shapes after grouping them by shape
     '''
@@ -199,7 +185,7 @@ def eval_mul_dims(in_path, out_path, model_path, device, batch_size, model_arch,
     for shape in in_path_of_shape:
         # run function on every unique image shape
         eval(in_path_of_shape[shape], out_path_of_shape[shape],
-             model_path, device, batch_size, model_arch, mask, blend)
+             model_path, device, batch_size, mask, blend)
 
 
 def main():
@@ -207,23 +193,31 @@ def main():
 
     start_time = time.time()
 
-    # check if input is file or dir
-    if not os.path.isdir(opts.in_path):
-        full_in = [opts.in_path]
-        in_name = os.path.splitext(os.path.basename(opts.in_path))
-        out_name = str(in_name[0] + '_' + os.path.splitext(os.path.basename(opts.model_path))[0] + in_name[1])
-        full_out = [os.path.join(opts.out_path, out_name) if os.path.isdir(opts.out_path) else opts.out_path]
+    if opts.model_path == "all":
+        models = glob.glob('pre-trained_models/*.ckpt')
     else:
-        # get all file names if dir
-        files = []
-        for (dir_path, dir_names, file_names) in os.walk(opts.in_path):
-            files.extend(file_names)
+        models = [opts.model_path]
 
-        full_in = [os.path.join(opts.in_path, x) for x in files]
-        full_out = [os.path.join(opts.out_path, x) if os.path.isdir(opts.out_path) else opts.out_path for x in files]
+    for m, model in enumerate(models):
 
-    eval_mul_dims(full_in, full_out, opts.model_path, device=opts.device, batch_size=opts.batch_size,
-                  model_arch=opts.model_arch, mask=opts.mask, blend=opts.blend)
+        # check if input is file or dir
+        if not os.path.isdir(opts.in_path):
+            full_in = [opts.in_path]
+            in_name = os.path.splitext(os.path.basename(opts.in_path))
+            out_name = str(in_name[0] + '_' + os.path.splitext(os.path.basename(model))[0] + in_name[1])
+            full_out = [os.path.join(opts.out_path, out_name) if os.path.isdir(opts.out_path) else opts.out_path]
+        else:
+            # get all file names if dir
+            files = []
+            for (dir_path, dir_names, file_names) in os.walk(opts.in_path):
+                files.extend(file_names)
+
+            full_in = [os.path.join(opts.in_path, x) for x in files]
+            full_out = [os.path.join(opts.out_path, x) if os.path.isdir(opts.out_path) else opts.out_path for x in
+                        files]
+
+        eval_mul_dims(full_in, full_out, model, device=opts.device, batch_size=opts.batch_size,
+                      mask=opts.mask, blend=opts.blend)
 
     print("\n Painting done in %0.3f seconds ... Have a good day!\n" % (time.time() - start_time))
 
